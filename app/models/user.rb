9350b974684379277a0970591afcdf0a0de9058a
Samuel Lair <lair001@gmail.com>
2016-10-05 02:20:52 -0400

    implemented User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < Sinatra::Base
 	include Slugifiable::InstanceMethods
 	extend Slugifiable::ClassMethods
 
+	has_many :posts
+	has_many :threads, through: :posts
+	has_secure_password
+
 end
