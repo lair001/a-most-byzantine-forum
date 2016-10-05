d2ccab872359ffe65ec0cb0f4bb116c3d604a11b
Samuel Lair <lair001@gmail.com>
2016-10-05 03:20:21 -0400

    added has many users through posts test for Thread

--- spec/models/02_thread_spec.rb
+++ spec/models/02_thread_spec.rb
@@ -23,4 +23,17 @@ describe 'Thread' do
     expect(@user.updated_at).to be_a(DateTime)
   end
 
+  it 'has many users through posts' do 
+    @user1 = User.create(username: "test 123", email: "test123@aol.com", password: "test", moderator: false, administrator: false)
+    @user2 = User.create(username: "igor", email: "igor@aol.com", password: "yumyum", moderator: false, administrator: false)
+    @post1 = Post.create(content: "ipsum lorem", user_id: @user1.id, thread_id: @thread.id)
+    @post2 = Post.create(content: "blah blah", user_id: @user2.id, thread_id: @thread.id)
+    expect(@thread.posts).to be_a(Array)
+    expect(@thread.posts).to include(@post1)
+    expect(@thread.posts).to include(@post2)
+    expect(@thread.users).to be_a(Array)
+    expect(@thread.users).to include(@user1)
+    expect(@thread.users).to include(@user2)
+  end
+
 end
