fb51c10b1557f2af3c227bffd1cde0c34d64cc44
Samuel Lair <lair001@gmail.com>
2016-10-07 02:13:04 -0400

    attempt to implement /threads/:slug and forum_threads/show.erb view

--- app/controllers/forum_threads_controller.rb
+++ app/controllers/forum_threads_controller.rb
@@ -9,4 +9,15 @@ class ForumThreadsController < Controller
 		end
 	end
 
+	get '/forum_threads/:slug' do
+		if logged_in?
+			@thread = ForumThread.find_by_slug(params[:slug])
+			redirect '/threads' if @thread.nil?
+			sort_thread_posts
+			erb :'forum_threads/show'
+		else
+			redirect '/'
+		end
+	end
+
 end
