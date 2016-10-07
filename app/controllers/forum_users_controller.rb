4e8e657f9e5e0d3a2b9ac7e23fab0fcf2196ed0e
Samuel Lair <lair001@gmail.com>
2016-10-06 22:13:39 -0400

    added login and logout

--- app/controllers/forum_users_controller.rb
+++ app/controllers/forum_users_controller.rb
@@ -1,3 +1,30 @@
 class ForumUsersController < Controller
 
+	get '/login' do
+		if logged_in?
+			redirect '/threads'
+		else
+			erb :'users/login'
+		end
+	end
+
+	post '/login' do
+		@user = ForumUser.find_by(username: params[:username])
+		if @user && @user.authenticate(params[:password])
+			session[:user_id] = @user.id
+			redirect '/threads'
+		else
+			redirect '/'
+		end
+	end
+
+	get '/logout' do 
+		if logged_in?
+			session.clear
+			redirect '/login'
+		else
+			redirect '/'
+		end
+	end
+
 end
