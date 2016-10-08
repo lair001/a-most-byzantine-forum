bf7303adcbbe0a0cb131141ca81b22e39833010f
Samuel Lair <lair001@gmail.com>
2016-10-08 00:24:15 -0400

    implemented post deletion

--- app/controllers/forum_posts_controller.rb
+++ app/controllers/forum_posts_controller.rb
@@ -1,3 +1,21 @@
 class ForumPostsController < Controller
 
+
+	delete '/forum_posts' do
+		if logged_in?
+			@post = ForumThread.find(params[:forum_thread][:id])
+			redirect "#{params[:cached_route]}" if @post.nil? || (@post.forum_user != current_user && !moderator?)
+			@thread = @post.forum_thread
+			@post.delete
+			if @thread.posts.count == 0
+				@thread.delete
+				redirect '/threads'
+			else
+				redirect "#{params[:cached_route]}"
+			end
+		else
+			redirect '/'
+		end
+	end
+
 end
