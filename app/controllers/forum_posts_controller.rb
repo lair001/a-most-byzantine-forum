e322c16c9ede2762caba49105b524f4479eeaf5a
Samuel Lair <lair001@gmail.com>
2016-10-08 01:00:19 -0400

    implemented post creation

--- app/controllers/forum_posts_controller.rb
+++ app/controllers/forum_posts_controller.rb
@@ -1,5 +1,20 @@
 class ForumPostsController < Controller
 
+	get '/forum_posts/new/:slug' do
+		@thread = ForumThread.find_by_slug(:slug)
+		erb :'/forum_posts/create'
+	end
+
+	post 'forum_posts' do
+		@post = ForumPost.new
+		set_attributes(@post, params[:forum_post], ["content", "forum_user_id", "forum_thread_id"])
+		if @post.save
+			route_array = params[:cached_route].split("/")
+			redirect "/forum_threads/#{route_array.last}"
+		else
+			redirect "#{params[:cached_route]}"
+		end
+	end
 
 	delete '/forum_posts' do
 		if logged_in?
