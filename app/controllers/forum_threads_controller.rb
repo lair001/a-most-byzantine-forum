b738febb8b2e9c0bbd2a4bdeb00f50a701ca5484
Samuel Lair <lair001@gmail.com>
2016-10-08 02:26:25 -0400

    implemented thread creation

--- app/controllers/forum_threads_controller.rb
+++ app/controllers/forum_threads_controller.rb
@@ -9,6 +9,36 @@ class ForumThreadsController < Controller
 		end
 	end
 
+	get '/forum_threads/new' do 
+		if logged_in?
+			erb :'forum_threads/create'
+		else
+			redirect '/'
+		end
+	end
+
+	post '/forum_threads' do 
+		if logged_in?
+			@thread = ForumThread.new 
+			set_attributes(@thread, params[:forum_thread], ["title"])
+			if @thread.save
+				@post = ForumPost.new 
+				set_attributes(@post, params[:forum_post], ["content", "forum_user_id"])
+				@post.forum_thread = @thread 
+				if @post.save 
+					redirect "/forum_threads/#{@thread.slug}"
+				else
+					@thread.delete
+					redirect '/forum_threads/new'
+				end
+			else
+				redirect '/forum_threads/new'
+			end
+		else
+			redirect '/'
+		end
+	end
+
 	get '/forum_threads/:slug' do
 		if logged_in?
 			@thread = ForumThread.find_by_slug(params[:slug])
