21f553398429881ea648a85b2fef336659561fb3
Samuel Lair <lair001@gmail.com>
2016-10-09 07:34:11 -0400

    continuing to implement ForumUserController tests

--- spec/controllers/06_forum_users_controller_spec.rb
+++ spec/controllers/06_forum_users_controller_spec.rb
@@ -197,6 +197,192 @@ describe 'ForumUsersController' do
 
 	end
 
+	describe "get '/forum_users/:slug'" do
+
+		it 'redirects to / if not logged in' do 
+			get '/forum_users/val'
+			expect(last_response.status).to eq(302)
+		  	follow_redirect!
+		  	expect(last_response.status).to eq(200)
+		  	expect(last_request.path).to include("/")
+			expect(last_response.body).to include("A Most Byzantine Forum")
+		end
+
+		it "renders the user's profile page if logged in" do
+			params = {
+		  		forum_user: { username: "val", password: "val" }
+		  	}
+		  	post '/login', params
+			get '/forum_users/val'
+		  	expect(last_response.status).to eq(200)
+		  	expect(last_request.path).to include("/forum_users/val")
+		  	expect(last_response.body).to include("val")
+		end
+
+	end
+
+	describe "get '/forum_users/:slug/edit'" do 
+
+		it 'redirects to / if not logged in' do 
+			get '/forum_users/val/edit'
+			expect(last_response.status).to eq(302)
+		  	follow_redirect!
+		  	expect(last_response.status).to eq(200)
+		  	expect(last_request.path).to include("/")
+			expect(last_response.body).to include("A Most Byzantine Forum")
+		end
+
+		it "redirects to / if accessing another user's edit page when not an administrator" do 
+			params = {
+		  		forum_user: { username: "sal", password: "sal" }
+		  	}
+		  	post '/login', params
+			get '/forum_users/val/edit'
+			expect(last_response.status).to eq(302)
+		  	follow_redirect!
+		  	expect(last_response.status).to eq(200)
+		  	expect(last_request.path).to include("/")
+			expect(last_response.body).to include("A Most Byzantine Forum")
+		end
+
+		it "allows an administrator to access another user's edit page" do 
+			params = {
+		  		forum_user: { username: "wal", password: "wal" }
+		  	}
+		  	post '/login', params
+			get '/forum_users/sal/edit'
+			expect(last_response.status).to eq(200)
+			expect(last_request.path).to include("/forum_users/sal/edit")
+			expect(last_response.body).to include("Edit")
+		end
+
+		it "allows a non-administrator to access his or her own edit page" do 
+			params = {
+		  		forum_user: { username: "hal", password: "hal" }
+		  	}
+		  	post '/login', params
+			get '/forum_users/hal/edit'
+			expect(last_response.status).to eq(200)
+			expect(last_request.path).to include("/forum_users/hal/edit")
+			expect(last_response.body).to include("Edit")
+		end
+
+		describe "patch '/forum_users'" do
+
+			it 'redirects to / if not logged in' do 
+				patch '/forum_users'
+				expect(last_response.status).to eq(302)
+			  	follow_redirect!
+			  	expect(last_response.status).to eq(200)
+			  	expect(last_request.path).to include("/")
+				expect(last_response.body).to include("A Most Byzantine Forum")
+			end
+
+			it 'redirects to /forum_users if user is successfully updated' do 
+				@user = ForumUser.create(username: "frank", email: "frank@frank.com", password: "frank")
+				params = {
+		  			forum_user: { username: "frank", password: "frank" }
+		  		}
+		  		post '/login', params
+		  		params = { forum_user: { id: @user.id, email: "frank@gmail.com" } }
+		  		patch '/forum_users', params 
+		  		expect(last_response.status).to eq(302)
+			  	follow_redirect!
+			  	expect(last_response.status).to eq(200)
+			  	expect(last_request.path).to include("/forum_users")
+				expect(last_response.body).to include("Users")
+			end
+
+			it 'allows a moderator to ban a user but cannot change username, email or password' do 
+				@user = ForumUser.create(username: "frank", email: "frank@frank.com", password: "frank")
+				id = @user.id
+				params = {
+		  			forum_user: { username: "sal", password: "sal" }
+		  		}
+		  		post '/login', params
+		  		params = { forum_user: { id: @user.id, username: "hank", email: "hank@hank.com", password: "hank", banned: true } }
+		  		patch '/forum_users', params 
+			  	follow_redirect!
+			  	@user = ForumUser.find(id)
+		  		expect(@user.username).to eq("frank")
+		  		expect(@user.email).to eq("frank@frank.com")
+		  		expect(@user.banned).to eq(true)
+		  		expect(@user.authenticate("frank")).to eq(@user)
+		  		expect(@user.authenticate("hank")).to eq(false)
+			end
+
+			it 'allows a moderator to unban a user' do 
+				@user = ForumUser.create(username: "frank", email: "frank@frank.com", password: "frank", banned: true)
+				id = @user.id
+				params = {
+		  			forum_user: { username: "sal", password: "sal" }
+		  		}
+		  		post '/login', params
+		  		params = { forum_user: { id: @user.id, banned: false } }
+		  		patch '/forum_users', params 
+			  	follow_redirect!
+			  	@user = ForumUser.find(id)
+		  		expect(@user.banned).to eq(false)
+			end
+
+			it "allows an administrator to change a user's username, email and password but cannot ban the user" do 
+				@user = ForumUser.create(username: "frank", email: "frank@frank.com", password: "frank")
+				id = @user.id
+				params = {
+		  			forum_user: { username: "wal", password: "wal" }
+		  		}
+		  		post '/login', params
+		  		params = { forum_user: { id: @user.id, username: "hank", email: "hank@hank.com", password: "hank", banned: true } }
+		  		patch '/forum_users', params 
+			  	follow_redirect!
+			  	@user = ForumUser.find(id)
+		  		expect(@user.username).to eq("hank")
+		  		expect(@user.email).to eq("hank@hank.com")
+		  		expect(@user.banned).to eq(false)
+		  		expect(@user.authenticate("frank")).to eq(false)
+		  		expect(@user.authenticate("hank")).to eq(@user)
+			end
+
+			it "allows an ordinary user to change his or her own email and password but not username or banned status" do 
+				@user = ForumUser.create(username: "frank", email: "frank@frank.com", password: "frank")
+				id = @user.id
+				params = {
+		  			forum_user: { username: "frank", password: "frank" }
+		  		}
+		  		post '/login', params
+		  		params = { forum_user: { id: @user.id, username: "hank", email: "hank@hank.com", password: "hank", banned: true } }
+		  		patch '/forum_users', params 
+			  	follow_redirect!
+			  	@user = ForumUser.find(id)
+		  		expect(@user.username).to eq("frank")
+		  		expect(@user.email).to eq("hank@hank.com")
+		  		expect(@user.banned).to eq(false)
+		  		expect(@user.authenticate("frank")).to eq(false)
+		  		expect(@user.authenticate("hank")).to eq(@user)
+			end
+
+			it "does not allow an ordinary user to change anything about another user" do 
+				@user = ForumUser.create(username: "frank", email: "frank@frank.com", password: "frank")
+				id = @user.id
+				params = {
+		  			forum_user: { id: @user.id, username: "hal", password: "hal" }
+		  		}
+		  		post '/login', params
+			  	follow_redirect!
+			  	@user = ForumUser.find(id)
+		  		params = { id: @user.id, forum_user: { username: "hank", email: "hank@hank.com", password: "hank", banned: true } }
+		  		patch '/forum_users', params 
+		  		expect(@user.username).to eq("frank")
+		  		expect(@user.email).to eq("frank@frank.com")
+		  		expect(@user.banned).to eq(false)
+		  		expect(@user.authenticate("frank")).to eq(@user)
+		  		expect(@user.authenticate("hank")).to eq(false)
+			end
+
+		end
+
+	end
+
 	#   it 'redirects to /forum_threads if logged in' do 
  #      	visit '/login'
  #      	fill_in("username", with: "val")
