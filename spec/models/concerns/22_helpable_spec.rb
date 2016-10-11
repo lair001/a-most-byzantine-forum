077a15d70fc752a592335120c26f99532d071854
Samuel Lair <lair001@gmail.com>
2016-10-11 01:15:53 -0400

    finished tests for module Helpable

--- spec/models/concerns/22_helpable_spec.rb
+++ spec/models/concerns/22_helpable_spec.rb
@@ -18,7 +18,8 @@ class Helper
 		:thread_posts,
 		:slug,
 		:params,
-		:request 
+		:request,
+		:thread 
 	]
 
 	ATTR_ARRAY.each { |attr| attr_accessor attr }
@@ -42,10 +43,29 @@ describe 'Helpable' do
 
 	before do
 	  	@helper1 = Helper.new
-
+		@user1 = ForumUser.create(username: "val", email: "val@val.com", password: "val", moderator: true, administrator: true, id: 1)
 	  	@user2 = ForumUser.create(username: "hal", email: "hal@hal.com", password: "hal", id: 2)
 	  	@user3 = ForumUser.create(username: "sal", email: "sal@sal.com", password: "sal", moderator: true, id: 3)
 	  	@user4 = ForumUser.create(username: "wal", email: "wal@wal.com", password: "wal", administrator: true, id: 4)
+
+	  	@thread1 = ForumThread.create(title: "the worst first", id: 1)
+	  	@thread2 = ForumThread.create(title: "Michael Jackson is a very bad boy", id: 2)
+	  	@thread3 = ForumThread.create(title: "The Lion, the Witch, and Madeline Albright", id: 3)
+	  	@thread4 = ForumThread.create(title: "Hello and Goodbye, Phil")
+
+	  	@post1 = ForumPost.create(content: "asdf", forum_user_id: 2, forum_thread_id: 1)
+	  	@post2 = ForumPost.create(content: "Hal, do you want to be the first to be banned?", forum_user_id: 1, forum_thread_id: 1)
+	  	@post3 = ForumPost.create(content: "You insult my honor, my lady.", forum_user_id: 2, forum_thread_id: 1)
+	  	@post4 = ForumPost.create(content: "Let me do it!", forum_user_id: 3, forum_thread_id: 1)
+		@post5 = ForumPost.create(content: "Wait, my hand slipped. I swear!", forum_user_id: 2, forum_thread_id: 1)
+		@post6 = ForumPost.create(content: "I always wanted to change someone's username to Cthulhu.", forum_user_id: 4, forum_thread_id: 1)
+		@post7 = ForumPost.create(content: "Fiddlesticks", forum_user_id: 2, forum_thread_id: 1)
+
+		@user_array = ForumUser.all.sort { |a, b| a.username <=> b.username }
+		@user2_posts_array = @user2.forum_posts.sort { |a, b| b.updated_at <=> a.updated_at }
+
+	  	@thread_array = ForumThread.all.sort { |a, b| b.updated_at <=> a.updated_at }
+	  	@thread1_posts_array = @thread1.forum_posts.sort { |a, b| a.created_at <=> b.created_at }
 	end
 
 	describe '#logged in' do 
@@ -146,7 +166,7 @@ describe 'Helpable' do
   	describe '#set_attributes' do 
 
   		it 'it sets attributes of an object that are permitted to be set using values in a hash' do 
-  			@user1 = ForumUser.create(username: "val", email: "val@val.com", password: "val", moderator: true, administrator: true, id: 1)
+  			@user5 = ForumUser.create(username: "phil", email: "phil@phil.com", password: "phil", moderator: true, administrator: true, id: 5)
   			attr_hash = { username: "bill", email: "bill@bill.com", password: "bill", banned: true }
   			settable_attr_array = ["username", "email", "password"]
   			@helper1.set_attributes(@user1, attr_hash, settable_attr_array)
@@ -161,6 +181,43 @@ describe 'Helpable' do
   			expect(@user1.email).to eq("will@will.com")
   			expect(@user1.password).to eq("will")
   			expect(@user1.banned).to eq(true)
+  		end
+
+  	end
+
+  	describe '#sort_users' do 
+
+  		it 'sorts users alphabetically' do 
+			@helper1.sort_users
+			expect(@helper1.users).to eq(@user_array)
+  		end
+
+  	end
+
+  	describe '#sort_threads' do 
+
+  		it 'sorts threads based on time when last updated (recently updated threads come first)' do 
+			@helper1.sort_threads
+			expect(@helper1.threads).to eq(@thread_array)
+  		end
+
+  	end
+
+  	describe '#sort_user_posts' do 
+
+  		it "sorts a user's posts based on time when last updated (recently updated threads come first)" do 
+			@helper1.sort_user_posts(@user2)
+			expect(@helper1.user_posts).to eq(@user2_posts_array)
+  		end
+
+  	end
+
+  	describe '#sort_thread_posts' do 
+
+  		it "sorts a user's posts based on time when last updated (recently updated threads come first)" do 
+  			@helper1.thread = @thread1
+			@helper1.sort_thread_posts
+			expect(@helper1.thread_posts).to eq(@thread1_posts_array)
   		end
 
   	end
