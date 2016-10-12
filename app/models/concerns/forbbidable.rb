651f607cebe5afa8d88ec4061855a07cb0748d48
Samuel Lair <lair001@gmail.com>
2016-10-12 06:58:50 -0400

    tweaking of allowed characters

--- app/models/concerns/forbbidable.rb
+++ app/models/concerns/forbbidable.rb
@@ -2,7 +2,7 @@ module Forbiddable
 	module InstanceMethods 
 
 		def absence_of_forbidden_characters(attribute)
-			errors.add(:base, "#{attribute} cannot have forbidden characters.") if self.send(attribute).is_a?(String) && self.send(attribute).match(/[^\w \-,.?}{!#&$|%+=\]\['><"):(;@~*\/\\^`Ā-ɏΑ-Ωα-ω₠-₾]+/)
+			errors.add(:base, "#{attribute} cannot have forbidden characters.") if self.send(attribute).is_a?(String) && self.send(attribute).match(/[^!-Ͽἀ-῾₠-₾]+/)
 		end
 
 	end
