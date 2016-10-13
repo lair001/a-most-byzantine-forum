9e30e17157ad5b6344dcfa76728dd1092d3382ef
Samuel Lair <lair001@gmail.com>
2016-10-13 01:41:18 -0400

    don't forget whitespace . . .

--- app/models/concerns/forbbidable.rb
+++ app/models/concerns/forbbidable.rb
@@ -2,7 +2,7 @@ module Forbiddable
 	module InstanceMethods 
 
 		def absence_of_forbidden_characters(attribute)
-			errors.add(:base, "#{attribute} cannot have forbidden characters.") if self.send(attribute).is_a?(String) && self.send(attribute).match(/[^!-ϿԱ-֏ἀ-῾₠-₾]+/)
+			errors.add(:base, "#{attribute} cannot have forbidden characters.") if self.send(attribute).is_a?(String) && self.send(attribute).match(/[^\s!-ϿԱ-֏ἀ-῾₠-₾]+/)
 		end
 
 	end
