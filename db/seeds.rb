b8fa27da308912071930978239965be284d3bd48
Samuel Lair <lair001@gmail.com>
2016-10-16 02:30:49 -0400

    abstracted seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,76 +1,62 @@
-ForumUser.create(username: "lair001", email: "lair001@hotmail.com", password: "lair001", moderator: true, administrator: true, id: 1)
-sleep 2
-ForumUser.create(username: "lair002", email: "lair002@hotmail.com", password: "lair002", moderator: true, administrator: true, id: 2)
-sleep 2
-ForumUser.create(username: "lair003", email: "lair003@hotmail.com", password: "lair003", moderator: true, id: 3)
-sleep 2
-ForumUser.create(username: "lair004", email: "lair004@hotmail.com", password: "lair004", id: 4)
-sleep 2
-ForumUser.create(username: "lair005", email: "lair005@hotmail.com", password: "lair005", id: 5)
-sleep 2
 
-ForumThread.create(title: "first thread", id: 1)
-sleep 2
-ForumThread.create(title: "second thread", id: 2)
-sleep 2
-ForumThread.create(title: "third thread", id: 3)
-sleep 2
-ForumThread.create(title: "fourth thread", id: 4)
-sleep 2
-ForumThread.create(title: "fifth thread", id: 5)
-sleep 2
+def create_users(num_of_superusers, num_of_adminstrators, number_of_moderators, num_of_ordinary_users, num_of_banned_users, delay)
 
-ForumPost.create(content: "posting first in the first thread", forum_user_id: 1, forum_thread_id: 1)
-sleep 2
-ForumPost.create(content: "posting first in the second thread", forum_user_id: 2, forum_thread_id: 2)
-sleep 2
-ForumPost.create(content: "posting first in the third thread", forum_user_id: 3, forum_thread_id: 3)
-sleep 2
-ForumPost.create(content: "posting first in the fourth thread", forum_user_id: 4, forum_thread_id: 4)
-sleep 2
-ForumPost.create(content: "posting first in the fifth thread", forum_user_id: 5, forum_thread_id: 5)
-sleep 2
+	user_array = []
+	i = 1
 
-ForumPost.create(content: "posting second in the first thread", forum_user_id: 2, forum_thread_id: 1)
-sleep 2
-ForumPost.create(content: "posting second in the second thread", forum_user_id: 3, forum_thread_id: 2)
-sleep 2
-ForumPost.create(content: "posting second in the third thread", forum_user_id: 4, forum_thread_id: 3)
-sleep 2
-ForumPost.create(content: "posting second in the fourth thread", forum_user_id: 5, forum_thread_id: 4)
-sleep 2
-ForumPost.create(content: "posting second in the fifth thread", forum_user_id: 1, forum_thread_id: 5)
-sleep 2
+	while i <= num_of_superusers + num_of_adminstrators + number_of_moderators + num_of_ordinary_users + num_of_banned_users do 
+		if i <= num_of_superusers
+			user_array << ForumUser.create(username: "lair00#{i}", email: "lair00#{i}@lair00#{i}.com", password: "lair00#{i}", moderator: true, administrator: true)
+		elsif i <= num_of_superusers + num_of_adminstrators
+			user_array << ForumUser.create(username: "lair00#{i}", email: "lair00#{i}@lair00#{i}.com", password: "lair00#{i}", administrator: true)
+		elsif num_of_superusers + num_of_adminstrators + number_of_moderators
+				user_array << ForumUser.create(username: "lair00#{i}", email: "lair00#{i}@lair00#{i}.com", password: "lair00#{i}", moderator: true)
+		elsif i <= num_of_superusers + num_of_adminstrators + number_of_moderators + num_of_ordinary_users
+			user_array << ForumUser.create(username: "lair00#{i}", email: "lair00#{i}@lair00#{i}.com", password: "lair00#{i}")
+		else
+			user_array << ForumUser.create(username: "lair00#{i}", email: "lair00#{i}@lair00#{i}.com", password: "lair00#{i}", banned: true)
+		end
+		sleep delay
+		i += 1
+	end
 
-ForumPost.create(content: "posting third in the first thread", forum_user_id: 3, forum_thread_id: 1)
-sleep 2
-ForumPost.create(content: "posting third in the second thread", forum_user_id: 4, forum_thread_id: 2)
-sleep 2
-ForumPost.create(content: "posting third in the third thread", forum_user_id: 5, forum_thread_id: 3)
-sleep 2
-ForumPost.create(content: "posting third in the fourth thread", forum_user_id: 1, forum_thread_id: 4)
-sleep 2
-ForumPost.create(content: "posting third in the fifth thread", forum_user_id: 2, forum_thread_id: 5)
-sleep 2
+	[user_array, delay]
 
-ForumPost.create(content: "posting fourth in the first thread", forum_user_id: 4, forum_thread_id: 1)
-sleep 2
-ForumPost.create(content: "posting fourth in the second thread", forum_user_id: 5, forum_thread_id: 2)
-sleep 2
-ForumPost.create(content: "posting fourth in the third thread", forum_user_id: 1, forum_thread_id: 3)
-sleep 2
-ForumPost.create(content: "posting fourth in the fourth thread", forum_user_id: 2, forum_thread_id: 4)
-sleep 2
-ForumPost.create(content: "posting fourth in the fifth thread", forum_user_id: 3, forum_thread_id: 5)
-sleep 2
+end
 
-ForumPost.create(content: "posting fifth in the first thread", forum_user_id: 5, forum_thread_id: 1)
-sleep 2
-ForumPost.create(content: "posting fifth in the second thread", forum_user_id: 1, forum_thread_id: 2)
-sleep 2
-ForumPost.create(content: "posting fifth in the third thread", forum_user_id: 2, forum_thread_id: 3)
-sleep 2
-ForumPost.create(content: "posting fifth in the fourth thread", forum_user_id: 3, forum_thread_id: 4)
-sleep 2
-ForumPost.create(content: "posting fifth in the fifth thread", forum_user_id: 4, forum_thread_id: 5)
-sleep 2
+def create_threads(users_delay_array, num_of_threads)
+
+	thread_array = []
+	i = 0
+
+	while i < num_of_threads do 
+		@user = users_delay_array[0][rand(1..users_delay_array[0].length)-1]
+		thread_array << ForumThread.create(title: "Thread ##{i+1}")
+		ForumPost.create(content: "Post ##{1} in Thread ##{i+1}", forum_user_id: @user.id, forum_thread_id: thread_array.last.id)
+		sleep users_delay_array[1]
+		i += 1
+	end
+
+	[users_delay_array[0], thread_array, users_delay_array[1]]
+
+end
+
+def create_posts(users_threads_delay_array, num_of_posts)
+
+	i = 0
+
+	while i < num_of_posts do 
+		@user = users_threads_delay_array[0][rand(1..users_threads_delay_array[0].length)-1]
+		@thread = users_threads_delay_array[1][rand(1..users_threads_delay_array[1].length)-1]
+		ForumPost.create(content: "Post ##{@thread.forum_posts.count + 1} in Thread #{@thread.id}", forum_user_id: @user.id, forum_thread_id: @thread.id)
+		sleep users_threads_delay_array[2]
+		i += 1
+	end
+
+end
+
+def seed_database(num_of_superusers, num_of_adminstrators, number_of_moderators, num_of_ordinary_users, num_of_banned_users, num_of_threads, num_of_posts, delay)
+	create_posts(create_threads(create_users(num_of_superusers, num_of_adminstrators, number_of_moderators, num_of_ordinary_users, num_of_banned_users, delay), num_of_threads), num_of_posts)
+end
+
+seed_database(1, 1, 1, 5, 2, 20, 80, 2)
