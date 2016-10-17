1802346f8fc29e06bc3b0f5c8e14145ed0731612
Samuel Lair <lair001@gmail.com>
2016-10-16 20:09:07 -0400

    refactored ForumUsersController to use Helpable#set_and_save_attributes

--- app/controllers/forum_users_controller.rb
+++ app/controllers/forum_users_controller.rb
@@ -56,8 +56,7 @@ class ForumUsersController < Controller
 
 	post '/forum_users' do 
 		@user = ForumUser.new
-		set_attributes(@user, trim_whitespace(params[:forum_user], ["username"]), ["username", "email", "password"])
-		if @user.save
+		if set_and_save_attributes(@user, trim_whitespace(params[:forum_user], ["username"]), ["username", "email", "password"])
 			session[:forum_user_id] = @user.id 
 			redirect '/forum_threads'
 		else
@@ -119,8 +118,7 @@ class ForumUsersController < Controller
 			settable_attr_array = []
 			settable_attr_array << "banned" if moderator?
 			administrator? ? settable_attr_array.push("username", "email", "password") : (settable_attr_array.push("email", "password") if @user == current_user)
-			set_attributes(@user, trim_whitespace(params[:forum_user], ["username"]), settable_attr_array)
-			if @user.save
+			if set_and_save_attributes(@user, trim_whitespace(params[:forum_user], ["username"]), settable_attr_array)
 				current_user.update(updated_at: Time.now) if administrator? || moderator?
 				redirect '/forum_users'
 			else
