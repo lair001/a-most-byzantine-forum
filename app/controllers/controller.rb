db0df0bd2114c472cfa165e8efc5802678e611d5
Samuel Lair <lair001@gmail.com>
2016-10-21 06:46:07 -0400

    moved public folder to app/assets to make the project more rails-like

--- app/controllers/controller.rb
+++ app/controllers/controller.rb
@@ -1,7 +1,7 @@
 class Controller < Sinatra::Base
 
 	configure do
-		set :public_folder, 'public'
+		set :public_folder, 'app/assets'
 		set :views, 'app/views'
 		enable :sessions
 		set :session_secret, "manzikert"
