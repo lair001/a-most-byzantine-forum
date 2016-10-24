825d7874b633406d5cfb5cdff1997aefb66af4a7
Samuel Lair <lair001@gmail.com>
2016-10-24 01:13:37 -0400

    added ForumPost callbacks to update thread after post creation and updating

--- app/models/forum_post.rb
+++ app/models/forum_post.rb
@@ -14,13 +14,24 @@ class ForumPost < ActiveRecord::Base
 		absence_of_forbidden_characters_in :content
 	end
 
-	after_create :set_creating_user_activity
+	after_create :update_creating_user_activity
+	after_create :update_thread_after_post_creation
+
 	after_update :update_current_user_activity
+	after_update :update_thread_after_post_update
 
 private
 
-	def set_creating_user_activity
+	def update_creating_user_activity
 		self.forum_user.update(last_active: Time.now)
+	end
+
+	def update_thread_after_creation
+		self.forum_thread.update(updated_at: Time.now) if self.forum_thread.posts.count > 1
+	end
+
+	def update_thread_after_post_update
+		self.forum_thread.update(updated_at: Time.now)
 	end
 
 end
