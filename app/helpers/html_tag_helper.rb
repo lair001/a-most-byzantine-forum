53fc98c6289958e8fd1953ea5bb7baeddea21a89
Samuel Lair <lair001@gmail.com>
2016-10-26 21:46:01 -0400

    refactored HTML helpers

--- app/helpers/html_tag_helper.rb
+++ app/helpers/html_tag_helper.rb
@@ -1,27 +1,28 @@
 module HtmlTagHelper
 
-	def link_to(link_text, url, html_options_hash = nil)
-		html = "<a href='#{url}'#{html_options_hash ? build_html_options(html_options_hash) : ""}>#{link_text}"
-		html += "#{yield}" if block_given?
-		html += "</a>"
+	def link_to(link_text, url, html_options = nil)
+		"<a href='#{url}' #{html_options ? build_html_options(html_options) : ""}>#{link_text}#{block_given? ? yield : ""}</a>"
 	end
 
-	def content_tag(tag_name, content, html_options_hash = nil)
-		html = "<#{tag_name}#{html_options_hash ? build_html_options(html_options_hash) : ""}>#{content}"
-		html += "#{yield}" if block_given?
-		html += "</#{tag_name}>"
+	def content_tag(tag_name, content, html_options = nil)
+		"<#{tag_name} #{html_options ? build_html_options(html_options) : ""}>#{content}#{block_given? ? yield : ""}</#{tag_name}>"
 	end
 
-	def tag(tag_name, html_options_hash = nil)
-		"<#{tag_name}#{html_options_hash ? build_html_options(html_options_hash) : ""} />"
+	def tag(tag_name, html_options = nil)
+		"<#{tag_name} #{html_options ? build_html_options(html_options) : ""} />"
 	end
 
-	def build_html_options(html_options_hash)
-		html_options = ""
-		html_options_hash.each do |key, value|
-			html_options += " #{key}='#{value}'"
-		end
-		html_options
+	# def build_html_options(html_options)
+	# 	binding.pry
+	# 	html_options = ""
+	# 	html_options.each do |key, value|
+	# 		html_options += " #{key}='#{value}'"
+	# 	end
+	# 	html_options
+	# end
+
+	def build_html_options(html_options)
+		html_options.is_a?(Hash) ? html_options.inspect.gsub(/[}{:>,]/, '') : html_options
 	end
 
 end
